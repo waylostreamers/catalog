68ec1d961fd3d0a20f234187e9c2405a584084ac
Cam Sexton <cameron.t.sexton@gmail.com>
2020-05-02 15:06:30 -0700

    fix formatting

--- content/models.py
+++ content/models.py
@@ -7,15 +7,9 @@ class User(models.Model):
     last_name = models.TextField()
 
 
-
-
-
-
 class Artist(models.Model):
-    name=models.TextField()
+    name = models.TextField()
     artwork_id = models.UUIDField(null=True)
-
-
 
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
